4328517d20b40e6711743fe8ba7b66fbd5aa3372
Gustavo Rodrigues Gutierrez <mmpguty.5@gmail.com>
2026-04-01 16:08:20 -0400

    Ajustado para aceitar de qualquer origem

--- src/main/java/com/relatech/backend/security/SecurityConfig.java
+++ src/main/java/com/relatech/backend/security/SecurityConfig.java
@@ -57,10 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://relatech.vercel.app"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
